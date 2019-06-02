8c6a71c3073b0752387d095f9479295f3815a44e
Terry (Yan) Wang <enjoybusy@hotmail.com>
2019-06-02 20:58:39 +1000

    FinishedAuthFlow

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there again" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 43 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
