060bc413f80bb02953a7d6ff11026899bdf14aac
Terry (Yan) Wang <enjoybusy@hotmail.com>
2019-06-17 15:58:34 +1000

    UpdateSecretKeyProd

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
